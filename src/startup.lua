5195cec79fe49b38115abe0da38724e82cf27ea0
MageCoven <casper.lindberg.github@gmail.com>
2025-07-17 19:26:43 +0200

    feat: added names to tabs

--- src/startup.lua
+++ src/startup.lua
@@ -5,7 +5,14 @@ global = {
     message_queue = PersistentQueue.new("message_queue")
 }
 
-shell.openTab("receive.lua")
+local function openTabWithName(script_path, name)
+    local tab = shell.openTab(script_path)
+    multishell.setTitle(tab, name)
+    return tab
+end
+
+openTabWithName("receive.lua", "Receiver")
+
 if fs.exists("worker.lua") then
-    shell.run("worker.lua")
+    openTabWithName("worker.lua", "Worker")
 end
