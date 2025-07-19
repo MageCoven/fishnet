b07f068dba65bac1d60b8f9b5fd8f0675817e6ab
MageCoven <casper.lindberg.github@gmail.com>
2025-07-19 18:42:21 +0200

    fix: removed old unused code in startup

--- src/startup.lua
+++ src/startup.lua
@@ -1,8 +1,3 @@
-local PersistentQueue = require("lib.persistent_queue")
-
----@diagnostic disable-next-line: lowercase-global
-global = {
-    message_queue = PersistentQueue.new("message_queue")
-}
-
-shell.run("worker.lua")
+if fs.exists("worker.lua") then
+    shell.run("worker.lua")
+end
